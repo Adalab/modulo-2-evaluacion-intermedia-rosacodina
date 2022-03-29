68b613a3b899a39a9854e07a22869c3e5585abd5
Rosa Codina <rosa.codina90@gmail.com>
2022-03-29 10:30:32 +0200

    html structure done and javaScript does not work at all

---
+++ main.js
@@ -0,0 +1,17 @@
+'user strict';
+//VARIABLES
+let balance = 50;
+let chosenNumber = 0;
+let betAmount = 0;
+
+//CONSTANTES
+const option1 = document.querySelector('.js-one');
+option1.addEventListener('click', () => {
+  console.log('Alerta');
+});
+
+function getRandomNumber(max {
+    min = 
+    max =
+  return Math.ceil(Math.random() * max);
+}
